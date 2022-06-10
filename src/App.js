87efa9581e945fbe4b8f7b045271538440d40ade
tolanchik1987 <tolanchik1987@mail.ru>
2022-06-10 07:51:11 +0400

    my first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import Profile from "./components/Profile";
+import Header from "./components/Header";
+import Navbar from "./components/Navbar";
+
+const App = () => {
+   return (
+      <div className="app-wrapper">
+         <Header />
+         <Navbar />
+         <Profile />
+      </div>
+   );
+};
+
+export default App;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,18 @@
+const Profile = () => {
+    return (
+        <div className="content">
+            <div>
+               <img className="img_1" src="https://www.utu.fi/sites/default/files/styles/hero_xl/public/media/drupal/matt-hardy-6ArTTluciuA-unsplash.jpg?itok=YyCLXXb9"/>
+            </div>
+            <div>
+               <img className="img_2" src="https://interactive-examples.mdn.mozilla.net/media/cc0-images/grapefruit-slice-332-332.jpg" />
+            </div>
+            <div>ava</div>
+            <div>mypost</div>
+            <div>newpost</div>
+            <div>post</div>
+        </div>
+    );
+};
+
+export default Profile;
