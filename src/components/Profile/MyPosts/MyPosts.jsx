526e14b589bf3c0a4c0a0c9ae2f91e64042c7850
tolanchik1987 <tolanchik1987@mail.ru>
2022-06-13 19:55:47 +0400

    creat props Post

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,3 +1,4 @@
+import Post from "../Post/Post";
 import classes from "./MyPosts.module.css";
 
 const MyPosts = () => {
@@ -6,10 +7,12 @@ const MyPosts = () => {
             <div className={classes.item}>ava</div>
             <div className={classes.item}>
                 <p>Mypost</p>
-                <p><textarea name="comment" cols="160" rows="3"></textarea></p>
+                <textarea cols="160" rows="3"></textarea>
+                <button>Add post</button>
             </div>
             <div className={classes.item}>newpost</div>
-            <div className={classes.item}>post</div>
+            <Post message="How i You?"/>
+            <Post message="Whot is your name?"/>
         </div>
     );
 };
---
+++ src/components/Profile/Post/Post.jsx
@@ -0,0 +1,15 @@
+import classes from "./Post.module.css";
+
+const Post = (props) => {
+    return (
+        <div className={classes.item}>
+             <img src="https://publicdomainvectors.org/tn_img/Male-Avatar-2.webp"/>
+             {props.message}
+             <div>
+                <button><img src="https://pngimg.com/uploads/like/like_PNG14.png"/>Like</button>
+             </div>
+        </div>
+    )
+}
+
+export default Post;
